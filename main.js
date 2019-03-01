4e9911db587a9ba41ee3794a0889221b49e155ab
Milos1993 <36367558+Milos1993@users.noreply.github.com>
2019-03-01 19:10:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function ukljuceno() {
+    document.getElementById('overlay').style.display = "block";
+}
+
+
+    document.querySelector('span').addEventListener('click', function() {
+        document.getElementById('overlay').style.display = "none";
+    })
